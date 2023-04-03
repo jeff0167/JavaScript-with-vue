a1e40e41b844cdecfaf9f6dc70c53415a4ba3e21
Jefferen <73894404+jeff0167@users.noreply.github.com>
2023-04-03 21:50:50 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -11,5 +11,12 @@ const app = Vue.createApp({
             this.wordTable[1] = this.word.toLowerCase();
             this.wordTable[2] = this.word.toUpperCase();
         },
+        vibrate(){ 
+         navigator.vibrate(100); // literally that easy
+        },
+ 
+        vibratePattern(){ 
+            navigator.vibrate([300,100,300,100]); 
+        }   
     }
 })
